0a3faf21abc990bdb7841a3e886be711749e5124
PaulDeveloper73 <kisakyepaul73@gmail.com>
2025-04-10 16:18:24 +0300

    Modified the about page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,6 +1,13 @@
 const AboutUs = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen">AboutUs</div>
+    <div className="flex items-center justify-center min-h-screen">
+      <h1>Welcome TO E-Shop</h1>
+      <p>
+        Uganda's No.1 Online Shopping Mall ➜ Buy Electronics, TVs,
+        Phones,Fashion, Supermarket & more from top brands ✓ Best prices in
+        Uganda ✓ Online shop you
+      </p>
+    </div>
   );
 };
 
